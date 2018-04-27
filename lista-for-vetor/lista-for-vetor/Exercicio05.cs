decf3602f80157f15c2c9913132c77c1c2ffcdff
Alan eduardo alves <alaneduardoalves2018@gmail.com>
2018-04-27 16:25:45 -0300

    Exercício 05 for com vetor
    Foi armazenado a quantidade e nome dos jogos e no final apresentado as quantidades de cada jogo.

---
+++ lista-for-vetor/lista-for-vetor/Exercicio05.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lista_for_vetor
+{
+    class Exercicio05
+    {
+        public Exercicio05()
+        {
+            Console.Write("Quantidade de jogos que deseja cadastrar: ");
+            int quantidadeDeRegistros = Convert.ToInt32(Console.ReadLine());
+
+            string[] nomeJogos = new string[quantidadeDeRegistros];
+            int[] quantidadeDeJogos = new int[quantidadeDeRegistros];
+
+            for (int i = 0; i < nomeJogos.Length; i++)
+            {
+                Console.Write("Nome do jogo: ");
+                nomeJogos[i] = Console.ReadLine();
+                Console.Write("Quantidade do jogo: ");
+                quantidadeDeJogos[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+
+            for (int i = 0; i < nomeJogos.Length; i++)
+            {
+                Console.WriteLine(nomeJogos[i] + " tem " + quantidadeDeJogos[i] + " unidades");
+            }
+
+
+
+        }
+    }
+}
