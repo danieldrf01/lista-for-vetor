27ae753120d44ba7c8d05644cdf7a8e727b84487
Alan eduardo alves <alaneduardoalves2018@gmail.com>
2018-04-27 14:16:32 -0300

    Exercício02 com for e vetor

---
+++ lista-for-vetor/lista-for-vetor/Exercicio02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lista_for_vetor
+{
+    class Exercicio02
+    {
+        public Exercicio02()
+        {
+            int[] idade = new int[7];
+
+            for (int i = 0; i < idade.Length; i++)
+            {
+                Console.WriteLine("Digite a idade: ");
+                idade[i] = Convert.ToInt32(Console.ReadLine());
+                             
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < idade.Length; i++)
+            {
+                Console.WriteLine("idade: " + idade[i]);
+            }
+
+
+        }
+
+    }
+}
